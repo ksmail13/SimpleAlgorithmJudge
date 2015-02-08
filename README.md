03f9a830df5837768dca8e419891e4a9d94b6e2d
Micky Kim <ksmail13@gmail.com>
2015-02-09 05:40:00 +0900

    Project Start
    create main page and server

---
+++ README.md
@@ -0,0 +1,4 @@
+# SimpleAlgorithmJudge
+v0.1 **start project**
+_ _ _
+**Still working**
---
+++ server_main.py
@@ -0,0 +1,58 @@
+import sys, os
+from functools import partial
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
+urlpath = {
+"" : "main.html"
+        }
+
+class JudgeRequestHandler(BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
+        print "Init request handler"
+        self.server_version = "uos_judge/0.2"
+    
+    def do_GET(self):
+        global urlpath
+        key = self.path[1:]
+        print "request get", key
+        url = key+".html" 
+        if os.path.exists(url):
+ 
+            self.send_response(200);
+            self.send_header("content-type", "text/html")
+            self.end_headers()
+            resp=''
+            for line in open(url, "r").readlines():
+                resp += line
+
+            self.wfile.write(resp.encode('utf-8'))
+        else :
+            self.send_error(404);
+
+    def do_POST(self):
+        global urlpath
+        key = self.path[1:]
+
+        print "request post", key
+
+        for k in self.headers:
+            self.send_response(200);
+            self.send_header("content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(k+"<br>")
+        self.wfile.write(self.headers["content-length"]);
+
+        self.wfile.write(self.rfile.read(int(self.headers["content-length"])));
+
+
+
+def main(argv):
+    try:
+        serv_obj = HTTPServer(('',8080), partial(JudgeRequestHandler))
+        serv_obj.serve_forever()
+    except KeyboardInterrupt:
+        print "Server exited"
+
+if __name__=="__main__":
+    main(sys.argv)
