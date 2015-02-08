36e924b88b68943aa91304ceb189c0594f150918
Micky Kim <ksmail13@gmail.com>
2015-02-09 06:00:57 +0900

    todo planning
    Todo planning post content parsing

--- server_main.py
+++ server_main.py
@@ -5,7 +5,11 @@ from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 urlpath = {
 "" : "main.html"
         }
-
+# TODO : create xml for create url mapping table
+# TODO : create transfrom template
+# TODO : db connection
+# TODO : session
+# TODO : judge logic
 class JudgeRequestHandler(BaseHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
@@ -43,7 +47,23 @@ class JudgeRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(k+"<br>")
         self.wfile.write(self.headers["content-length"]);
 
-        self.wfile.write(self.rfile.read(int(self.headers["content-length"])));
+        self.content_parsing(self.rfile.read(int(self.headers["content-length"])));
+
+        if self.req_params is not None:
+            pass
+
+    def complie_and_execute(self, lang, code):
+        """
+        compile code check recv result and execute recv result data
+        """
+        pass
+
+
+    def content_parsing(self, contents):
+        self.req_params = {}
+        for attr in contents.split('&'):
+            attr_key, attr_value = attr.split("=")
+            req_params[attr_key] = attr_value
 
 
 
