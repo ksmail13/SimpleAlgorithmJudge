c6571a035d3950cac1910b20ba165a3d06fbc50f
Micky Kim <ksmail13@me.com>
2015-05-19 02:20:26 +0900

    socket close

---
+++ JudgeServer/PollManager.cc
@@ -0,0 +1,62 @@
+#include "PollManager.h"
+#include "logger.h"
+void PollManager::init(PollHandler &handler, bool isEdge = false)
+{
+    if(isEdge) throw "Not ready";
+
+    this->_handler = &handler;
+    _ed = epoll_create(PollManager::MAX_POLL);
+
+    if (_ed == -1) {
+        ErrorMessage("epoll_create() error");
+        return ;
+    }
+}
+
+
+bool PollManager::addEvent(int fd, int events = EPOLLIN)
+{
+    struct epoll_event ev;
+
+    ev.events = events;
+    ev.data.fd = fd;
+    
+    return epoll_ctl(_ed, EPOLL_CTL_ADD, fd, &ev) != -1;
+}
+
+bool PollManager::removeEvent(int fd)
+{
+    return epoll_ctl(_ed, EPOLL_CTL_DEL, fd, NULL) != -1;
+}
+
+
+void PollManager::onEvent(const struct epoll_event &event)
+{
+    if(event.events & EPOLLIN) {
+        _handler->onRead(event.data.fd);
+    }
+    if(event.events & EPOLLOUT) {
+        _handler->onWrite(event.data.fd);
+    }
+    if(event.events & EPOLLERR) {
+        _handler->onError(event.data.fd);
+    }
+}
+
+void PollManager::wait(int timeout)
+{
+    int nevs, i;
+    struct epoll_event evs[MAX_POLL] = {0,};
+    while(true) {
+        nevs = epoll_wait(_ed, evs, MAX_POLL, timeout);
+
+        if(nevs == -1) {
+            ErrorMessage("epoll_wait() error");
+            break;
+        }
+
+        for(i=0;i<nevs;i++) {
+            onEvent(evs[i]);
+        }
+    }
+}
--- JudgeServer/main.cc
+++ JudgeServer/main.cc
@@ -1,7 +1,45 @@
 #include <iostream>
+#include "InetSocket.h"
+#include "PollManager.h"
 #include "logger.h"
 
 using namespace std;
+using namespace Network;
+
+class JudgeServer: public PollHandler
+{
+    public:
+        JudgeServer *getInstance() {
+            if(instance == null)
+                instance = new JudgeServer();
+            return instance;
+        }
+
+        void run();
+
+        virtual void onRead(int fd);
+        virtual void onWrite(int fd);
+        virtual void onError(int fd);
+    private:
+        static JudgeServer *instance;
+        PollManager _pm;
+        JudgeServer() {_pm.init(*this);}
+}
+
+void JudgeServer::onRead(int fd)
+{
+    
+}
+
+void JudgeServer::onWrite(int fd)
+{
+    
+}
+
+void JudgeServer::onError(int fd)
+{
+    
+}
 int main()
 { 
 	InformMessage("information %d", 1);
