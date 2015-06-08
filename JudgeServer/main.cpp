fb6637c286ffc54c768dcba14de31c644bf196c3
micky <ksmail13@me.com>
2015-06-08 10:20:37 +0900

    server

--- JudgeServer/main.cpp
+++ JudgeServer/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "JudgeServer.h"
 #include "logger.h"
+#include <unistd.h>
+#include <signal.h>
 
 using namespace std;
 using namespace Network;
@@ -10,7 +12,8 @@ using namespace Network;
 
 int main(int argc, const char *argv[])
 {
-    InformMessage("information %d", 1);
+    //InformMessage("information %d", 1);
+    signal(SIGPIPE, SIG_IGN);
     JudgeServer *server = JudgeServer::getInstance();
     if(server != NULL) server->run();
 
